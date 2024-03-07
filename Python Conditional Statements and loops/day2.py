9eecb534d0b7827ef1adedcb24c223a5ec7a0616
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2024-03-07 22:09:06 +0530

    Update day2.py

--- Python Conditional Statements and loops/day2.py
+++ Python Conditional Statements and loops/day2.py
@@ -61,3 +61,65 @@ if x==1:
     print("Well guessed!")
 else:
     print("thank you")
+
+
+# Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+# Note : Use 'continue' statement.
+# Expected Output : 0 1 2 4 5
+
+for i in range(0,6):
+    if i==3:
+        continue
+    else:
+        print(i)
+
+
+
+# Write a Python program to get the Fibonacci series between 0 and 50.
+# Note : The Fibonacci Sequence is the series of numbers :
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+# Every next number is found by adding up the two numbers before it.
+# Expected Output : 1 1 2 3 5 8 13 21 34
+
+list=[0,1]
+# list.append(0)
+print(list)
+sum=0
+
+for i in range(0,50):
+    sum=list[i]+list[i+1]
+    list.append(sum)
+print(*list)
+
+print("second method")
+
+a,b=0,1
+print(a,end=",")
+i=0
+while i<=50:
+    print(b,end=",")
+    a,b=b,a+b
+    i=i+1
+
+
+# Write a Python program that iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of three and five, print "FizzBuzz".
+# Sample Output :
+# fizzbuzz
+# 1
+# 2
+# fizz
+# 4
+# buzz
+
+a='fizzbuzz'
+b='fizz'
+c='buzz'
+for i in range(0,50):
+    if (i%3)==0 and (i%5)==0:
+        print(a)
+    elif (i%3)==0:
+        print(b)
+    elif (i%5)==0:
+        print(c)
+    else:
+        print(i)
